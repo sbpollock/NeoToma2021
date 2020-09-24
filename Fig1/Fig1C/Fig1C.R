6c49964d52dc27982ab9fc3e61e3f6ed61fa7306
sbpollock <30759271+sbpollock@users.noreply.github.com>
2020-09-24 16:07:06 -0700

    Update Fig1C.R

--- Fig1/Fig1C/Fig1C.R
+++ Fig1/Fig1C/Fig1C.R
@@ -1,11 +1,12 @@
 # Load packages and set working directory
 library(tidyverse)
-library(googlesheets4)
+library(rstudioapi)
+current_path <- getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
 
 # Import data
-url <- "https://docs.google.com/spreadsheets/d/1CABinImKsHcVbICivbfMPJfiyd3V0Kr6sIqd-5GofWw/edit#gid=0"
-sht <- read_sheet(url,sheet="overview") %>% filter(Use=="Yes") 
-sht2 <- sht %>% select("Peptide_Count"=MHC_Peptides,Cell_Line,Species,"Cell_Count"=`Effective injection(cell equiv.)`)
+sht <- read.csv("data_peptide_counts.csv")
+sht2 <- sht %>% select("Peptide_Count"=MHC_Peptides,Cell_Line,Species,"Cell_Count"=Cell_Equivalents)
 
 sht2$Peptide_Count <- as.numeric(sht2$Peptide_Count)
 sht2$Cell_Count <-as.numeric(sht2$Cell_Count)
@@ -30,4 +31,4 @@ guides(size = guide_legend(override.aes = list(fill="black")))+
 guides(fill = guide_legend(override.aes = list(size=5)))
 
 # Save image
-ggsave(path="/Volumes/RLadies/Manuscript_AssayMAP/Figures/Fig1",filename="Fig1C.png", width = 3.5, height = 5,plot=p,units = "in")
+ggsave(filename="Fig1C.png",width = 3.5, height = 5)

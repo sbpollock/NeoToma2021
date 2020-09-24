f8c6608404b87f35ecbd7711fbcc4adabfeca976
sbpollock <30759271+sbpollock@users.noreply.github.com>
2020-09-24 15:45:55 -0700

    Update Fig1E.R

--- Fig1/Fig1E/Fig1E.R
+++ Fig1/Fig1E/Fig1E.R
@@ -1,12 +1,14 @@
+# Load packages and set working directory
 library(tidyverse)
 library(rstudioapi)
-
 current_path <- getActiveDocumentContext()$path 
 setwd(dirname(current_path ))
 
+# Import data
 sht <- read.csv("data_reuse.csv") %>% 
 mutate(Elution=as.factor(Elution))
 
+# Plot data
 p <- ggplot(sht,aes(x=Elution,y=Count,fill=Type,color=Type))+
 stat_summary(aes(group=Type),fun = "mean", geom = "line",size=3)+
 scale_y_continuous(limits = c(0,3000))+
@@ -17,6 +19,7 @@ axis.title = element_text(size=14),
 legend.text = element_text(size=14),
 legend.title = element_text(size=14))
 
+# Save image
 ggsave(filename="Fig1E.png",
 plot=p,
 width = 5, height = 4)

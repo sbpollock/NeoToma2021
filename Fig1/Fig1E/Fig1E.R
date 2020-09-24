050f499504a47811c45d72cdf591e22faf6b4f5c
sbpollock <30759271+sbpollock@users.noreply.github.com>
2020-09-24 15:43:23 -0700

    Update Fig1E.R

--- Fig1/Fig1E/Fig1E.R
+++ Fig1/Fig1E/Fig1E.R
@@ -1,8 +1,11 @@
 library(tidyverse)
-library(googlesheets4)
-url <- "https://docs.google.com/spreadsheets/d/1ckQf5HFtVQqYj7j0HsYKJ5TcdXlXNoB-0TxZDguELd8/edit#gid=743349605"
-sht <- read_sheet(url,sheet="result - reuse") %>% 
-  mutate(Elution=as.factor(Elution))
+library(rstudioapi)
+
+current_path <- getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
+
+sht <- read.csv("data_reuse.csv") %>% 
+mutate(Elution=as.factor(Elution))
 
 p <- ggplot(sht,aes(x=Elution,y=Count,fill=Type,color=Type))+
 stat_summary(aes(group=Type),fun = "mean", geom = "line",size=3)+
@@ -14,6 +17,6 @@ axis.title = element_text(size=14),
 legend.text = element_text(size=14),
 legend.title = element_text(size=14))
 
-ggsave(filename="/Volumes/RLadies/Manuscript_AssayMAP/Figures/Fig1/Fig1E.png",
+ggsave(filename="Fig1E.png",
 plot=p,
 width = 5, height = 4)

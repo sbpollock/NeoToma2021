8e4ae9bc62b76f5a5843d7e17689882cb03734c6
sbpollock <30759271+sbpollock@users.noreply.github.com>
2020-12-13 17:03:26 -0800

    Update Fig2D.R

--- Fig2/Fig2D/Fig2D.R
+++ Fig2/Fig2D/Fig2D.R
@@ -1,3 +1,56 @@
+# Load packages
+library(tidyverse)
+library(googlesheets4)
+library(reshape2)
+
+# Import data
+sheet <- read.csv("./oxidation.csv")
+
+# Separate TIC by Ox %
+Ox_percent <- as.factor(c(0,0.01,0.05,0.1,0.25,0.5,1,1.5))
+
+df0 <- subset(sheet,Ox_percent==0)
+df0.01 <- subset(sheet,Ox_percent==0.01)
+df0.05 <- subset(sheet,Ox_percent==0.05)
+df0.1 <- subset(sheet,Ox_percent==0.1)
+df0.25 <- subset(sheet,Ox_percent==0.25)
+df0.5 <- subset(sheet,Ox_percent==0.5)
+df1 <- subset(sheet,Ox_percent==1)
+df1.5 <- subset(sheet,Ox_percent==1.5)
+
+TIC <- c(
+sum(df0$Area),
+sum(df0.01$Area),
+sum(df0.05$Area),
+sum(df0.1$Area),
+sum(df0.25$Area),
+sum(df0.5$Area),
+sum(df1$Area),
+sum(df1.5$Area)
+)
+
+df_ox <- data.frame(Ox_percent,TIC) %>% 
+  mutate(log10=log(TIC,base=10))
+      
+# Filter dataframe for Neoantigens
+clean <- function(df) {
+  df %>%
+    filter(str_detect(Accession,"NEO_")) %>%
+    filter(!str_detect(Accession,"#DECOY")) %>%
+    filter(!str_detect(Accession,"sp")) %>%
+    filter(!str_detect(Accession,"tr")) %>%
+    filter(Area > 0)
+}
+
+df0_c <- clean(df0)
+df0.01_c <- clean(df0.01)
+df0.05_c <- clean(df0.05)
+df0.1_c <- clean(df0.1)
+df0.25_c <- clean(df0.25)
+df0.5_c <- clean(df0.5)
+df1_c <- clean(df1)
+df1.5_c <- clean(df1.5)
+
 ############################################ 
 ## % unoxidized vs. sulfoxide vs. sulfone ##
 ############################################
